452090634d7c10b822d078b49eb01c0295e453a0
JY ABLE <okoudje@outlook.com>
2020-07-08 14:44:03 +0200

    ok

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "azurerm_app_service" "spacegame_dev" {
 
   site_config {
     linux_fx_version = "DOTNETCORE|3.1"
-    #app_command_line = "dotnet Tailspin.SpaceGame.Web.dll"
+    app_command_line = "dotnet Tailspin.SpaceGame.Web.dll"
   }
 }
 
